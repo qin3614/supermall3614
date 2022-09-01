d87be75d84b46b77bb5c193901aa632481b52e24
unknown <3614655010@qq.com>
2022-09-01 16:19:07 +0800

    sssss

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h2>我是app组件</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+@import "./assets/css/base.css";
+</style>
